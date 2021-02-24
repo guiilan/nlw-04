aa15451676e978aeb205fb3bf4f80e3e9862fac6
guiilan <gui.tioze@gmail.com>
2021-02-24 00:06:01 -0300

    Adicionando painel e countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,24 @@
-import { profile } from "console";
+import { Countdown } from "../components/Countdown";
+import { CompletedChallenges } from "../components/CompletedChallenges";
 import { ExperienceBar } from "../components/ExperienceBar";
-import { Profile } from '../components/Profile'
+import { Profile } from '../components/Profile';
 
-import styles from '../styles/Pages/Home.module.css'
+import Head from 'next/head';
+
+import styles from '../styles/Pages/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio - move.it</title>
+      </Head>
       <ExperienceBar />
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
+          <Countdown />
         </div>
         <div>
 
